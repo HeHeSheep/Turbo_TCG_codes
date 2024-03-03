8b920e0133ea89b9d28e3f2d90ce63a849ffae91
Tsz Yeung CHIN <1155176266@link.cuhk.edu.hk>
2024-03-04 03:43:51 +0800

    Create new Class card_info

--- crawl_from_WS/for_WS_Shopline.py
+++ crawl_from_WS/for_WS_Shopline.py
@@ -26,15 +26,47 @@ import bs4
 import re
 import csv
 import re
-
+class card_info:
+    def __init__(self, card_name, card_no, neo_standard_define,
+                 title_no, rare, side, card_type,
+                 color, level, cost, power, soul, trigger,
+                 attribute, text, favour_text, card_image_url, next_card_url):
+        self.card_name = card_name
+        self.card_no = card_no
+        self.neo_standard_define = neo_standard_define
+        self.title_no = title_no
+        self.rare = rare
+        self.side = side
+        self.card_type = card_type
+        self.color = color
+        self.level = level
+        self.cost = cost
+        self.power = power
+        self.soul = soul
+        self.trigger = trigger
+        self.attribute = attribute
+        self.text = text
+        self.favour_text = favour_text
+        self.card_image_url = card_image_url
+        self.next_card_url = next_card_url
 
 def write_line_into_csv_shopline():
-
     return 0
 
 
+def fetch_html(url):
+    html_file = requests.get(url)  # fetch the html file from the url
+    if html_file.status_code != 200:  # check if the webpage is downloaded successfully
+        print(f'網頁拿取失敗，狀態為:{html_file.status_code}')# 輸出error code
+        return 0
+    objSoup = bs4.BeautifulSoup(html_file.text, 'html.parser')  # 將html轉成BeautifulSoup物件
+    # Here may need sth to check if I am going to correct webpage
+
+    return objSoup
+
+
 def main():
-# Define Constant
+    # Define Constant
     pattern = r'^(.+)/(W|S)(E?)([0-9]+)-([0-9+])$'
 
     print("歡迎使用WS 官網卡片爬蟲")
@@ -43,17 +75,20 @@ def main():
     print("請輸入目標彈數第一張卡")
 
     set_no = ""
-    while(True):
-    # 加入Regex 檢查輸入是否正確
+    while True:
+        # 加入Regex 檢查輸入是否正確
         set_no = input("例子：PJS/S109-113")
         if re.match(pattern, set_no):
+            # Check if this input goes to where unexpected:
+            url = f'https://ws-tcg.com/cardlist/?cardno={set_no}'
+            soup = fetch_html(url)
+
             break
         else:
             # 睇下之後有冇時間加function 去解䆁點解not match.
             print("輸入不符合要求，請重新輸入。")
     # Maybe need a sample output for first card.
     # As there have no way for me to check whether
-
 
     return 0
 
