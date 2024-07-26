a325ac3607fe4f305c406b73c6d18f6531e8eacd
Tsz Yeung CHIN <1155176266@link.cuhk.edu.hk>
2024-07-27 05:08:40 +0800

    Dynamic change content by js

--- result_report_website/Frontend_Design/JavaScript/match_result_report_page.js
+++ result_report_website/Frontend_Design/JavaScript/match_result_report_page.js
@@ -23,16 +23,30 @@
 //             console.error('Error:', error);
 //         });
 // });
-let win_player_id = "test001"
+
+var roundNumberVariable = "1" // TODO: make them dynamic changed based on received data from DB
+var tableNumberVariable = "1" // TODO: make them dynamic changed based on received data from DB
+var playerNameVariable = "test001" // TODO: make them dynamic changed based on received data from DB
+
+document.getElementById('header').innerText = "Result Upload of Table " + tableNumberVariable;
+document.getElementById('roundNo').innerText = "Round No.: " + roundNumberVariable;
+document.getElementById('tableNo').innerText = "Table No.: " + tableNumberVariable;
+document.getElementById('playerName').innerText = "Player Name: " + playerNameVariable;
+var callAPI = (/* TODO: fill in with variables that have to pass, e.g. player name*/) =>{
+
+}
+
+const win_player_id = "test001"; // TODO: make test001 dynamic
 document.getElementById('submit_button').addEventListener('click', function() {
     const selectedOption = document.getElementsByName('result_choices')[0].value;
     if (selectedOption === "NULL"){
     }
     else if (selectedOption === "you_win"){
-        window.alert("Report: " + win_player_id + " Win")
+        window.alert("Reported: " + win_player_id + " Win")
+
     }
     else if (selectedOption === "double_defeated"){
-        window.alert("Report: Double Defeated")
+        window.alert("Reported: Double Defeated")
     }
     else{
         window.alert("Not Recognized select value. Refreshing this page...")
